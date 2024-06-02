d830593755a325e8c815fa8d83641c2d992fd4d1
Pavlo Kolodka <pavelkolodka.dev@gmail.com>
2024-06-02 12:25:09 +0300

    Add C# examples

---
+++ C# examples/FrontController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace FrontControllerPattern
+{
+    public class View
+    {
+        public void Show()
+        {
+            Console.WriteLine("Displaying Home Page");
+        }
+    }
+
+    public class Dispatcher
+    {
+        private readonly HomeController _homeController;
+        private readonly ErrorController _errorController;
+
+        public Dispatcher()
+        {
+            _homeController = new HomeController();
+            _errorController = new ErrorController();
+        }
+
+        public void Dispatch(string request)
+        {
+            if (request.Equals("HOME", StringComparison.OrdinalIgnoreCase))
+            {
+                _homeController.ProcessRequest();
+            }
+            else
+            {
+                _errorController.ProcessRequest();
+            }
+        }
+    }
+
+    public interface IController
+    {
+        void ProcessRequest();
+    }
+
+    public class HomeController : IController
+    {
+        private readonly View _view = new View();
+
+        public void ProcessRequest()
+        {
+            _view.Show();
+        }
+    }
+
+    public class ErrorController : IController
+    {
+        public void ProcessRequest()
+        {
+            Console.WriteLine("Displaying Error Page");
+        }
+    }
+
+    public class FrontController
+    {
+        private readonly Dispatcher _dispatcher;
+
+        public FrontController()
+        {
+            _dispatcher = new Dispatcher();
+        }
+
+        public void HandleRequest(string request)
+        {
+            _dispatcher.Dispatch(request);
+        }
+    }
+
+    static class Program
+    {
+        public static void RunExample()
+        {
+            FrontController frontController = new FrontController();
+
+            frontController.HandleRequest("HOME");
+            frontController.HandleRequest("ERROR");
+        }
+    }
+}
---
+++ C# examples/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Builder pattern: ");
+
+            BuilderPattern.Program.RunExample();
+
+            Console.WriteLine("Front Controller pattern: ");
+
+            FrontControllerPattern.Program.RunExample();
+
+            Console.WriteLine("Null Object pattern: ");
+
+            NullObjectPattern.Program.RunExample();
+
+            Console.WriteLine("Thread-Specific Storage pattern: ");
+
+            ThreadSpecificStoragePattern.Program.RunExample();
+        }
+    }
+}
+
